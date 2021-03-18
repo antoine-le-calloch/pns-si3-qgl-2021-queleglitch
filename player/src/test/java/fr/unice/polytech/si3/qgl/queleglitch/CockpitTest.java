9615c5aaa3617391f8ecdcced42b7de6c3bf52ba
Le Calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2021-03-18 12:35:16 +0100

    #43 correction erreur maven des tests avec l'ouverture de fichier, changement ouverture des fichier avec getRessource

--- player/src/test/java/fr/unice/polytech/si3/qgl/queleglitch/CockpitTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/queleglitch/CockpitTest.java
@@ -1,4 +1,4 @@
-/*package fr.unice.polytech.si3.qgl.queleglitch;
+package fr.unice.polytech.si3.qgl.queleglitch;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,43 +7,61 @@ import fr.unice.polytech.si3.qgl.queleglitch.json.goal.RegattaGoal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
 class CockpitTest {
 
-    FileOpener fileOpener;
     Cockpit cockpitGame2;
+    String path;
 
     @BeforeEach
     void setUp() {
-        fileOpener = new FileOpener("src\\test\\java\\fr\\unice\\polytech\\si3\\qgl\\queleglitch\\txtJson\\test");
+        path = "txtJson\\test";
 
         cockpitGame2 = new Cockpit();
-        cockpitGame2.initGame(fileOpener.getTxtInFile("2\\initGameFile"));
+        cockpitGame2.initGame(getTxtInFile("2\\initGameFile"));
     }
 
     @Test
     void Test_input1() {
         Cockpit cockpitGame1 = new Cockpit();
-        cockpitGame1.initGame(fileOpener.getTxtInFile("1\\initGameFile"));
-        String actualOutput = cockpitGame1.nextRound(fileOpener.getTxtInFile("1\\nextRoundFile"));
+        cockpitGame1.initGame(getTxtInFile("1\\initGameFile"));
+        String actualOutput = cockpitGame1.nextRound(getTxtInFile("1\\nextRoundFile"));
 
-        assertEquals(fileOpener.getTxtInFile("1\\outputFile"),actualOutput);
+        assertEquals(getTxtInFile("1\\outputFile"),actualOutput);
     }
 
     @Test
     void Test_input2_HowToReachCheckpoint2WEEK6() {
         ((RegattaGoal) cockpitGame2.informationGame.goal).numActualCheckpoint = 2;
 
-        String actualOutput = cockpitGame2.nextRound(fileOpener.getTxtInFile("2\\nextRoundFile"));
+        String actualOutput = cockpitGame2.nextRound(getTxtInFile("2\\nextRoundFile"));
 
-        assertEquals(fileOpener.getTxtInFile("2\\outputFile"),actualOutput);
+        assertEquals(getTxtInFile("2\\outputFile"),actualOutput);
     }
 
     @Test
     void Test_input2_HowToReachCheckpoint2WEEK6_NextRound() {
         ((RegattaGoal) cockpitGame2.informationGame.goal).numActualCheckpoint = 2;
 
-        String actualOutput = cockpitGame2.nextRound(fileOpener.getTxtInFile("2\\nextRoundFile_roundNext"));
+        String actualOutput = cockpitGame2.nextRound(getTxtInFile("2\\nextRoundFile_roundNext"));
 
-        assertEquals(fileOpener.getTxtInFile("2\\outputFile_roundNext"),actualOutput);
+        assertEquals(getTxtInFile("2\\outputFile_roundNext"),actualOutput);
     }
-}*/
+
+    private String getTxtInFile(String fileName){
+        try {
+            InputStream inputStream = CockpitTest.class.getResourceAsStream(path + fileName);
+            InputStreamReader isReader = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(isReader);
+            StringBuilder sb = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                sb.append(str);
+            }
+            return sb.toString().replaceAll("\\s", "");
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
