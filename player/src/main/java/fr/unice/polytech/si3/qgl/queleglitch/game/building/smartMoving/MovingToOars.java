3070af5380957f2938a14990e269515073c821c9
Antoine Huot-marchand <antoine.huot-marchand@etu.univ-cotedazur.fr>
2021-04-08 10:52:23 +0200

    #55 refactoring de MovingToOars

--- player/src/main/java/fr/unice/polytech/si3/qgl/queleglitch/game/building/smartMoving/MovingToOars.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/queleglitch/game/building/smartMoving/MovingToOars.java
@@ -22,35 +22,42 @@ public class MovingToOars {
     }
 
     public void movingAndUseOars(NbOarsUsed nbOarsUsed, List<Sailor> sailorsAvailable, List<Entities> entitiesTooFar, List<Action> actionsList){
-        Sailor sailorToMove = null;
-        for (int i = 0; i < nbOarsUsed.onLeft(); i++) {
-            for (int j = leftOars.size() - i - 1; j >= nbOarsUsed.onLeft() - i -1; j--) {
-                if((sailorToMove = tooling.nearestSailorBehind5(leftOars.get(j), sailorsAvailable)) != null) {
-                    actionsList.add(tooling.buildMovingAction(sailorToMove, leftOars.get(j)));
-                    actionsList.add(new Row(sailorToMove.getId()));
-                    sailorsAvailable.remove(sailorToMove);
-                    break;
-                }
-            }
-            if(sailorToMove == null) {
-                entitiesTooFar.add(leftOars.get((i == 0) ? nbOarsUsed.onLeft() : leftOars.size()-i));
-                break;
-            }
-        }
+        movingAndUseOarsOnSide(leftOars, nbOarsUsed, sailorsAvailable, entitiesTooFar, actionsList);
+        movingAndUseOarsOnSide(rightOars, nbOarsUsed, sailorsAvailable, entitiesTooFar, actionsList);
+    }
 
-        for (int i = 0; i < nbOarsUsed.onRight(); i++) {
-            for (int j = leftOars.size() - i - 1; j >= nbOarsUsed.onRight() - i - 1; j--) {
-                if((sailorToMove = tooling.nearestSailorBehind5(rightOars.get(j), sailorsAvailable)) != null) {
-                    actionsList.add(tooling.buildMovingAction(sailorToMove, rightOars.get(j)));
-                    actionsList.add(new Row(sailorToMove.getId()));
-                    sailorsAvailable.remove(sailorToMove);
+    public void AddToActionList(Sailor sailorToMove,List<Oar> sideOars, List<Action> actionsList, List<Sailor> sailorsAvailable, int j) {
+        actionsList.add(tooling.buildMovingAction(sailorToMove, sideOars.get(j)));
+        actionsList.add(new Row(sailorToMove.getId()));
+        sailorsAvailable.remove(sailorToMove);
+    }
+
+    public Sailor getNearestSailorBehind5OnThisSide(List<Oar> sideOars, List<Sailor> sailorsAvailable, int j){
+        return tooling.nearestSailorBehind5(sideOars.get(j),sailorsAvailable);
+    }
+
+    public int nbOarsUsedOnSide(List<Oar> sideOars,NbOarsUsed nbOarsUsed){
+        if(sideOars.equals(leftOars))
+            return nbOarsUsed.onLeft();
+        else if(sideOars.equals(rightOars))
+            return nbOarsUsed.onRight();
+        return 0;
+    }
+
+    public void movingAndUseOarsOnSide(List<Oar> sideOars, NbOarsUsed nbOarsUsed, List<Sailor> sailorsAvailable, List<Entities> entitiesTooFar, List<Action> actionsList){
+        Sailor sailorToMove = null;
+        for (int i = 0; i < nbOarsUsedOnSide(sideOars,nbOarsUsed); i++) {
+            for (int j = sideOars.size() - i - 1; j >= nbOarsUsedOnSide(sideOars,nbOarsUsed) - i -1; j--) {
+                if((sailorToMove = getNearestSailorBehind5OnThisSide(sideOars,sailorsAvailable,j)) != null) {
+                    AddToActionList(sailorToMove,sideOars,actionsList,sailorsAvailable,j);
                     break;
                 }
             }
             if(sailorToMove == null) {
-                entitiesTooFar.add(rightOars.get((i == 0) ? nbOarsUsed.onRight() : leftOars.size()-i));
+                entitiesTooFar.add(sideOars.get((i == 0) ? nbOarsUsed.onLeft() : sideOars.size()-i));
                 break;
             }
         }
     }
+
 }
