cbfe521d400457b87f9f95bab97046248ca52b36
LoicMadern <loic.madern@etu.univ-cotedazur.fr>
2021-05-06 12:34:08 +0200

    #57 test MovingWatch

--- player/src/test/java/fr/unice/polytech/si3/qgl/queleglitch/game/building/smartMoving/MovingToWatchTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/queleglitch/game/building/smartMoving/MovingToWatchTest.java
@@ -19,36 +19,53 @@ class MovingToWatchTest {
     Tooling mockTooling;
     Watch watch;
     Sailor sailor =new Sailor(1,1,1);
-    List<Sailor> sailors=new ArrayList<Sailor>() ;
-    List<Entities> entities = new ArrayList<Entities>();
-    List<Action> actions = new ArrayList<Action>();
-    Sailor sailorToMove;
+    List<Sailor> sailorsAvailablle =new ArrayList<Sailor>() ;
+    List<Sailor> sailorsAvailablleEmpty =new ArrayList<Sailor>() ;
+    List<Entities> entitiesTooFar = new ArrayList<Entities>();
+    List<Action> actionsList = new ArrayList<Action>();
+    List<Action> actionsListEmpty = new ArrayList<Action>();
 
     @BeforeEach
     void testUp(){
-        sailors.add(sailor);
+        sailorsAvailablle.add(sailor);
         mockTooling = Mockito.mock(Tooling.class);
         watch = new Watch();
-        Mockito.when(mockTooling.nearestSailorBehind5(watch,sailors)).thenReturn(new Sailor(1,1,1));
+        Mockito.when(mockTooling.nearestSailorBehind5(watch, sailorsAvailablle)).thenReturn(new Sailor(1,1,1));
         Mockito.when(mockTooling.buildMovingAction(null,watch)).thenReturn(null);
         movingToWatch = new MovingToWatch(watch,mockTooling);
     }
 
     @Test
     void noAvailableSailor(){
-        assertTrue(entities.isEmpty());
-        movingToWatch.movingAndUseWatch(sailors,entities,actions);
-        assertFalse(entities.isEmpty());
+        assertTrue(entitiesTooFar.isEmpty());
+        movingToWatch.movingAndUseWatch(sailorsAvailablle, entitiesTooFar, actionsList);
+        assertTrue(entitiesTooFar.isEmpty());
     }
       @Test
     void availableSailor(){
-        assertTrue(actions.isEmpty());
-        assertFalse(sailors.isEmpty());
-        movingToWatch.movingAndUseWatch(sailors,entities,actions);
-        assertTrue(sailors.isEmpty());
-        assertFalse(actions.isEmpty());
+        assertTrue(actionsList.isEmpty());
+        assertFalse(sailorsAvailablle.isEmpty());
+        movingToWatch.movingAndUseWatch(sailorsAvailablle, entitiesTooFar, actionsList);
+        assertTrue(sailorsAvailablle.isEmpty());
+        assertFalse(actionsList.isEmpty());
     }
 
+    @Test
+    void noSailors(){
+        assertTrue(actionsList.isEmpty());
+        assertTrue(sailorsAvailablleEmpty.isEmpty());
+        movingToWatch.movingAndUseWatch(sailorsAvailablleEmpty, entitiesTooFar, actionsList);
+        assertTrue(sailorsAvailablleEmpty.isEmpty());
+        assertTrue(actionsList.isEmpty());
+    }
+    @Test
+    void noActionsList(){
+        assertTrue(actionsListEmpty.isEmpty());
+        assertFalse(sailorsAvailablle.isEmpty());
+        movingToWatch.movingAndUseWatch(sailorsAvailablle, entitiesTooFar, actionsListEmpty);
+        assertTrue(sailorsAvailablle.isEmpty());
+        assertFalse(actionsListEmpty.isEmpty());
+    }
 
 
 }
